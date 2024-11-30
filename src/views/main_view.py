552da27168b763324074883a653c79875e4e37ad
Eddys912 <eddys912dev@gmail.com>
2024-11-30 00:18:27 -0600

    Interfaces de usuarios

--- src/views/main_view.py
+++ src/views/main_view.py
@@ -8,3 +8,26 @@ class Main:
         self.main.show()
 
         self.main.icon_bar.setHidden(True)
+
+        #seleccionar una pagina
+        self.main.btn_users.clicked.connect(self.page_users)
+        self.main.btn_products.clicked.connect(self.page_products)
+        self.main.btn_machine.clicked.connect(self.page_machine)
+        self.main.btn_boss.clicked.connect(self.page_boss)
+        self.main.btn_users_2.clicked.connect(self.page_users)
+        self.main.btn_products_2.clicked.connect(self.page_products)
+        self.main.btn_machine_2.clicked.connect(self.page_machine)
+        self.main.btn_boss_2.clicked.connect(self.page_boss)
+
+    def page_users(self):
+        self.main.stackedWidget.setCurrentIndex(0)
+
+    def page_products(self):
+        self.main.stackedWidget.setCurrentIndex(1)
+    
+    def page_machine(self):
+        self.main.stackedWidget.setCurrentIndex(2)
+    
+    def page_boss(self):
+        self.main.stackedWidget.setCurrentIndex(3)
+
