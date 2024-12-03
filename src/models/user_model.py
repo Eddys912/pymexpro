6c436efe7c0594903baa2db11fb6f40cbd0efb5d
Eddys912 <eddys912dev@gmail.com>
2024-12-02 18:24:25 -0600

    Modelo refactorizado, practicas clean code

--- src/models/user_model.py
+++ src/models/user_model.py
@@ -2,31 +2,7 @@ from src.database.connection import DBConnection
 
 
 class UserModel:
-    def __init__(
-        self,
-        first_name="",
-        last_name="",
-        gender="",
-        username="",
-        password="",
-        email="",
-        phone="",
-        role="",
-        is_active="True",
-        birth_date="",
-        registration_date="",
-    ):
-        self._first_name = first_name
-        self._last_name = last_name
-        self._gender = gender
-        self._username = username
-        self._password = password
-        self._email = email
-        self._phone = phone
-        self._role = role
-        self._is_active = is_active
-        self._birth_date = birth_date
-        self._registration_date = registration_date
+    def __init__(self):
         self.db_connection = DBConnection()
 
     def _execute_query(self, query, params=None, fetch_one=False):
@@ -41,8 +17,7 @@ class UserModel:
             conn.commit()
             return result
         except Exception as e:
-            print(f"Error al ejecutar la consulta: {e}")
-            return None
+            raise Exception(f"Error al ejecutar la consulta: {e}")
         finally:
             if cursor:
                 cursor.close()
@@ -52,89 +27,72 @@ class UserModel:
         query = """
             SELECT 
                 CONCAT(first_name, ' ', last_name) AS full_name, 
-                CASE 
-                    WHEN gender = 'M' THEN 'Masculino' 
-                    WHEN gender = 'F' THEN 'Femenino' 
-                    ELSE 'Otro' 
-                END AS gender,
+                gender,
                 username, 
                 email, 
-                phone, 
+                CONCAT(SUBSTR(phone, 1, 3), '-', SUBSTR(phone, 4, 3), '-', SUBSTR(phone, 7, 4)) AS formatted_phone,
                 role, 
-                is_active, 
+                CASE 
+                    WHEN is_active = 1 THEN 'Sí'
+                    ELSE 'No'
+                END AS is_active_status,
                 TIMESTAMPDIFF(YEAR, birth_date, CURDATE()) AS age
-            FROM users
+            FROM users;
+
         """
-        users = self._execute_query(query)
-        if users is None:
-            return {"message": "Error al obtener los usuarios"}
-        return users
+        return self._execute_query(query)
 
     def get_user_by_id(self, user_id):
         query = "SELECT * FROM users WHERE id = %s"
-        user = self._execute_query(query, (user_id,), fetch_one=True)
-        if user is None:
-            return {"message": f"Usuario con ID {user_id} no encontrado"}
-        return user
+        return self._execute_query(query, (user_id,), fetch_one=True)
 
-    def create_user(self):
+    def get_user_by_email(self, email):
+        query = "SELECT * FROM users WHERE email=%s"
+        return self._execute_query(query, (email,), fetch_one=True)
+
+    def get_user_by_username(self, username):
+        query = "SELECT * FROM users WHERE username=%s"
+        return self._execute_query(query, (username,), fetch_one=True)
+
+    def create_user(self, user_data):
         query = """
-            INSERT INTO users (first_name, last_name, gender, username, password, email, phone, role, is_active, birth_date, registration_date)
-            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+            INSERT INTO users (first_name, last_name, gender, username, password, email, phone, role, birth_date)
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
         """
         values = (
-            self._first_name,
-            self._last_name,
-            self._gender,
-            self._username,
-            self._password,
-            self._email,
-            self._phone,
-            self._role,
-            self._is_active,
-            self._birth_date,
-            self._registration_date,
+            user_data["first_name"],
+            user_data["last_name"],
+            user_data["gender"],
+            user_data["username"],
+            user_data["password"],
+            user_data["email"],
+            user_data["phone"],
+            user_data["role"],
+            user_data["birth_date"],
         )
-        result = self._execute_query(query, values)
-        if result is None:
-            return {"message": "Error al crear usuario"}
-        return {"message": "Usuario creado correctamente"}
 
-    def update_user(self, user_id):
-        user = self.get_user_by_id(user_id)
-        if "message" in user:
-            return user
+        return self._execute_query(query, values)
+
+    def update_user(self, user_id, user_data):
         query = """
             UPDATE users
-            SET first_name=%s, last_name=%s, gender=%s, username=%s, password=%s, email=%s, phone=%s, role=%s, is_active=%s, birth_date=%s, registration_date=%s
-            WHERE id = %s
+            SET first_name=%s, last_name=%s, gender=%s, username=%s, password=%s, email=%s, phone=%s, role=%s, is_active=%s, birth_date=%s WHERE id = %s
         """
         values = (
-            self._first_name,
-            self._last_name,
-            self._gender,
-            self._username,
-            self._password,
-            self._email,
-            self._phone,
-            self._role,
-            self._is_active,
-            self._birth_date,
-            self._registration_date,
+            user_data["first_name"],
+            user_data["last_name"],
+            user_data["gender"],
+            user_data["username"],
+            user_data["password"],
+            user_data["email"],
+            user_data["phone"],
+            user_data["role"],
+            user_data["is_active"],
+            user_data["birth_date"],
             user_id,
         )
-        result = self._execute_query(query, values)
-        if result is None:
-            return {"message": "Error al actualizar usuario"}
-        return {"message": "Usuario actualizado correctamente"}
+        return self._execute_query(query, values)
 
     def delete_user(self, user_id):
-        user = self.get_user_by_id(user_id)
-        if "message" in user:
-            return user
-
         query = "DELETE FROM users WHERE id = %s"
-        result = self._execute_query(query, (user_id,))
-        if result is None:
-            return {"message": "Error al eliminar usuario"}
-        return {"message": "Usuario eliminado correctamente"}
+        return self._execute_query(query, (user_id,))
