49217f29a8d3fc4098fc374b5542cd27e635ee5f
Eddys912 <eddys912dev@gmail.com>
2024-12-03 04:16:45 -0600

    correcion de los campos de la bd

--- src/database/create_tables.py
+++ src/database/create_tables.py
@@ -26,15 +26,16 @@ def create_table_users(conn):
                 user_id INT AUTO_INCREMENT PRIMARY KEY,
                 first_name VARCHAR(100) NOT NULL,
                 last_name VARCHAR(100) NOT NULL,
-                gender VARCHAR(10),
-                username VARCHAR(50) NOT NULL,
+                gender VARCHAR(10) NOT NULL,
+                username VARCHAR(50) NOT NULL UNIQUE,
                 password VARCHAR(255) NOT NULL,
                 email VARCHAR(100) NOT NULL UNIQUE,
-                phone VARCHAR(15),
+                phone VARCHAR(15) NOT NULL,
                 role VARCHAR(50) NOT NULL,
-                is_active BOOLEAN DEFAULT TRUE,
-                birth_date TIMESTAMP NULL,
-                registration_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+                birth_date DATETIME NOT NULL,
+                address VARCHAR(100) NOT NULL,
+                is_active TINYINT(1) DEFAULT 1,
+                registration_date DATETIME DEFAULT CURRENT_TIMESTAMP
             )
         """
         )
